75d5b3a7ef6c90c434f4f0fe0ab37b424972bf63
Saikiran M <Saikiran.M@outlook.in>
2023-03-28 23:50:45 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,61 @@
+variable "versioning" {} 
+
+variable "acl" {} 
+
+variable "bucket_name" {} 
+
+variable "bucket2_name" {} 
+
+variable "bucket3_name" {} 
+
+variable "encryption" {} 
+
+variable "ami" {} 
+
+variable "instance_id" {} 
+
+variable "ec2_name" {} 
+
+variable "ec2_2_name" {} 
+
+variable "vpc_id" {} 
+
+variable "subnet" {} 
+
+variable "subnet2" {} 
+
+variable "subnet3" {} 
+
+variable "ecs_name" {} 
+
+variable "container" {} 
+
+variable "service_name" {} 
+
+variable "family_ecs" {} 
+
+variable "launchtemplate" {} 
+
+variable "asg" {} 
+
+variable "eccp" {} 
+
+variable "engine" {} 
+
+variable "instance_class" {} 
+
+variable "storage_type" {} 
+
+variable "storage" {} 
+
+variable "user" {} 
+
+variable "pwd" {} 
+
+#variable "zones" {} 
+
+variable "db" {} 
+
+variable "dbversion" {} 
+
+ 
